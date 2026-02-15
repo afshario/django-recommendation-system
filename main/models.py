d4685c094d95107fa8c6ed0992b004aa0665e64a
MHAfshari <mhafshari1385@gmail.com>
2026-02-15 18:24:35 +0330

    model: add a user model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+      '''
+      this is a class to define users for rec_system
+      '''
+      username = models.CharField(max_length=250,unique=True)
+      email = models.EmailField(unique=True)
+      is_active = models.BooleanField(default= False)
+      is_staff = models.BooleanField(default= False)
+
+      USERNAME_FIELD = "email"
+      
--- rec/settings.py
+++ rec/settings.py
@@ -120,3 +120,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# User config
+AUTH_USER_MODEL = 'main.User'
